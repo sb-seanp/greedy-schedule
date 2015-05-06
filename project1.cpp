7c56dba46efb007c18a94146e0b645aef9f98fc0
Sean Prasertsit <seanp.usa@gmail.com>
2015-05-05 20:43:57 -0700

    Added comments and printing fixes.

--- project1.cpp
+++ project1.cpp
@@ -49,17 +49,23 @@ public:
 
 int compare (Job* a, Job* b);
 
+
+// Main function
 int main(int argc, char **argv) {
+
+    // Vector to hold jobs data
     vector<Job *> jobs;
+
+    // Parsing variables
     string word;
     string line;
     getline(cin,line);
-    int jobs_length = stoi(line);
 
     int index;
     int release;
     int size;
 
+    // Parse piped input
     while (getline(cin, line)) {
         istringstream ss(line);
         ss >> word;
@@ -73,34 +79,45 @@ int main(int argc, char **argv) {
         jobs.push_back(job);
     }
 
+    // Sort jobs vector
     sort(jobs.begin(), jobs.end(), compare);
+
+    // Two queues to handle choosing the right job
     priority_queue<Job*, vector<Job*>, QCompare> job_queue;
     priority_queue<Job*, vector<Job*>, QCompare> temp_queue;
-    vector<int> schedule;
-    queue<Job*> q_s;
 
+    // Vector to store final schedule
+    vector<int> schedule;
+
+    // Move from sorted vector into initial queue
     while (!jobs.empty()) {
         Job* temp = jobs.back();
         job_queue.push(temp);
         jobs.pop_back();
     }
 
+    // Time-related variables
     int time = 0;
+    int optimal = 0;
+
+    // Job scheduling algorithm
     while (!job_queue.empty()) {
         Job* top = job_queue.top();
+
+        // If it's at the lowest size (which it will by priority queue rules)
+        // and it can be released, add it to the schedule.
         if (top->getRelease() <= time) {
             schedule.push_back(top->getIndex());
-            q_s.push(top);
-            top->setSize(top->getSize() - 1);
-            cerr << top->getIndex() << " ";
-            cerr << top->getRelease() << " ";
-            cerr << top->getSize() << endl;
+            top->setSize(top->getSize() - 1); // Decrease size
+
+            // Put everything back into the main queue.
             while (!temp_queue.empty()) {
                 job_queue.push(temp_queue.top());
                 temp_queue.pop();
             }
             time++;
         }
+        // If no valid job is found, pop from main queue and move to temp queue.
         else {
             temp_queue.push(top);
             job_queue.pop();
@@ -115,62 +132,28 @@ int main(int argc, char **argv) {
         }
         if (top->getSize() == 0) {
             job_queue.pop();
-            continue;
+            optimal += time;
         }
     }
 
-    /*for (int i = 0; i < schedule.size(); i++) {
-        cout << schedule[i] << endl;
-    }*/
+    // Print the stuff.
+    cout << optimal << endl;
 
-    /*for (int i = 0; i < jobs_length; i++) {
-        Job *temp;
-        cout << job_queue.top()->getIndex() << " ";
-        cout << job_queue.top()->getRelease() << " ";
-        cout << job_queue.top()->getSize() << endl;
-        job_queue.pop();
-    }*/
+    for (int i = 0; i < schedule.size(); i++) {
+        cerr << schedule[i] << endl;
+    }
 
     return 0;
 }
 
-// Class Job functions
+// Job constructor
 Job::Job(int i, int r, int l) {
     index = i;
     release = r;
     size = l;
 }
 
+// Function that sorts jobs by release time
 int compare (Job* a, Job* b) {
     return ( a->getRelease() > b->getRelease() );
 }
-
-// Class ParseInput functions
-/*void pushJob(int i, int r, int l) {
-    Job *job = new Job(i, r, l);
-    jobs.push_back(job);
-}*/
-
-/*void ParseInput::parse() {
-    string line;
-    cin >> line;
-    int jobs_length;
-    stringstream(line) >> jobs_length;
-
-    int index;
-    int release;
-    int length;
-
-    int number;
-
-    while (getline(cin, line)) {
-        stringstream(line) >> number;
-        index = number;
-        stringstream(line) >> number;
-        release = number;
-        stringstream(line) >> number;
-        length = number;
-
-        pushJob(index, release, length);
-    }
-}*/
